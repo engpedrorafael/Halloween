874b39b8ca643f82d12fe5e2856ad80dddf81006
Pedro <engpedrorafael@gmail.com>
2017-11-01 23:07:47 +0000

    Fixed url

--- configure.sh
+++ configure.sh
@@ -14,11 +14,14 @@ echo "Installing Web Page..."
 sudo cp webPage/000-default.conf /etc/apache2/sites-available/
 sudo ls -s /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 
-sudo cp webPage/page.cgi /usr/lib/cgi-bin
+cat webPage/page.cgi | sed "s#%PATH%#$(pwd)#g" > webPage/page.cgi_installed
+sudo cp webPage/page.cgi_installed /usr/lib/cgi-bin/page.cgi
 sudo chmod 755 /usr/lib/cgi-bin
 sudo chown root.root /usr/lib/cgi-bin
 sudo chmod 775 /usr/lib/cgi-bin/page.cgi
-sudo cp webPage/index.html /var/www/html/
+
+cat webPage/index.html | sed "s#%IP%#$(hostname -I)#g" > webPage/index.html_installed
+sudo cp webPage/index.html_installed /var/www/html/indes.html
 sudo chown www-data.www-data /var/www/html/index.html
 
 echo "Restarting apache..."
--- webPage/page.cgi
+++ webPage/page.cgi
@@ -14,15 +14,15 @@ echo "parameter: ${parm[1]}"
 echo "</br>"
 
 if [[ "${parm[0]}" == "action" ]]; then
-    /home/gfi/Halloween/${parm[1]}.py 2>&1
+    %PATH%/${parm[1]}.py 2>&1
 elif [[ "${parm[0]}" == "sound" ]]; then
-    /home/gfi/Halloween/play.sh /home/gfi/Halloween/Sounds/${parm[1]}.mp3 2>&1
+    %PATH%/play.sh %PATH%/Sounds/${parm[1]}.mp3 2>&1
 elif [[ "${parm[0]}" == "stop" ]]; then
     if [[ "${parm[1]}" == "sound" ]]; then
         pkill -f vlc
     elif [[ "${parm[1]}" == "action" ]]; then
         pkill -f python
-        /home/gfi/Halloween/off.py 2>&1
+        %PATH%/off.py 2>&1
     fi
 else
     echo "Unknwon command/parameter"
